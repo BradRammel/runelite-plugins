9c2d73d5fc06b55a9d016ddcd350aedcc1301a1b
Brad Rammel <bmrammel@gmail.com>
2022-01-22 14:21:52 -0500

    Created bank value plugin

--- src/main/java/com/bankvalue/BankValuePlugin.java
+++ src/main/java/com/bankvalue/BankValuePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.bankvalue;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Bank Value"
 )
-public class ExamplePlugin extends Plugin
+public class BankValuePlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private BankValueConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	BankValueConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(BankValueConfig.class);
 	}
 }
